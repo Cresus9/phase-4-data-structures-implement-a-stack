e331d6539358ba417265303a9af4cca012659ab8
Sigui Thierry Herman Yabre <thierry.yabre@yahoo.fr>
2022-11-14 12:48:01 -0500

    commit tree 18bec270ee82984fea2bc571b5ed00a32414fef3

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,17 @@
-# your code here
+def Stack
+    def initialize(limit=nil)
+        @stack=[]
+        @limit=nil
+    end
+    def push(item)
+        raise 'Stack overflow' if full?
+        @stack.push(item)
+    end
+    def pop
+        @stack.pop
+    end
+    def peek
+        @stack.last
+    end
+
+end
